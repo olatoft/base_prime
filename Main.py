0d569ca640735eaf766e19c2bad8da074f662621
Ola Toft <olatoft@hotmail.com>
2017-04-03 19:48:13 +0200

    Added support for generating primes

--- Main.py
+++ Main.py
@@ -6,5 +6,27 @@ class Converter():
 	def convert(self, base_ten_number):
 		pass
 
+class Primes():
+
+	def __init__(self):
+		self.primes = [2, 3]
+
+	def generate_prime(self):
+		number = self.primes[-1] + 2
+		while True:
+			if self.is_prime(number):
+				self.primes.append(number)
+				return number
+			else:
+				number += 2
+			
+	def is_prime(self, number):
+		for divisor in self.primes:
+			if divisor**2 > number:
+				return True
+			elif number % divisor == 0:
+				return False
+
 if __name__ == '__main__':
-	pass
+	converter = Converter()
+	print (converter.convert(7))
--- Test.py
+++ Test.py
@@ -3,11 +3,32 @@ import Main
 
 class TestConvert(unittest.TestCase):
 
-	def setUp(self):
-		self.converter = Main.Converter()
+    def setUp(self):
+        self.converter = Main.Converter()
 
-	def test_convert(self):
-		self.assertEqual(self.converter.convert(7), 1000)
+    def test_normal_convert(self):
+        self.assertEqual(self.converter.convert(7), 1000)
+
+    def test_faulty_convert(self):
+        self.assertEqual(self.converter.convert(4), 'Error')
+
+class TestPrimes(unittest.TestCase):
+
+    def setUp(self):
+        self.primes = Main.Primes()
+
+    def test_is_prime(self):
+        self.assertEqual(self.primes.is_prime(4), False)
+        self.assertEqual(self.primes.is_prime(5), True)
+        self.assertEqual(self.primes.is_prime(6), False)
+        self.assertEqual(self.primes.is_prime(7), True)
+        self.assertEqual(self.primes.is_prime(8), False)
+        self.assertEqual(self.primes.is_prime(9), False)
+
+    def test_generate_prime(self):
+        self.assertEqual(self.primes.generate_prime(), 5)
+        self.assertEqual(self.primes.generate_prime(), 7)
+        self.assertNotEqual(self.primes.generate_prime(), 9)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
